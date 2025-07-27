374bb3cb51b161e63eba737106a48c527c25feaa
AntoineEales <antoineeales@icloud.com>
2025-07-27 21:51:06 +0200

    Update MainActivity.kt

--- app/app/src/main/app/src/main/java/com/anchorai/app/MainActivity.kt
+++ app/app/src/main/app/src/main/java/com/anchorai/app/MainActivity.kt
@@ -6,6 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.compose.material.Text
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,3 +25,5 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String) {
   Text(text = "Welcome to AnchorAI, $name!")
 }
+AppCenter.start(getApplication(), "{"389466b7-8853-490e-9faf-4f7d7f296852"}",
+                  Analytics.class, Crashes.class);
